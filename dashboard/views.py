71524329e2302a8a005af6d577664c2e92836668
namdevri <46358671+namdevri@users.noreply.github.com>
2019-09-04 16:11:00 -0400

    Update views.py
    Added 404 and 500 handlers

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def handler404(request, exception, template_name="404.html"):
+    response = render_to_response('404.html')
+    response.status_code = 404
+    return response
+
+def handler500(request, exception, template_name="500.html"):
+    response = render_to_response('500.html')
+    response.status_code = 500
+    return response
